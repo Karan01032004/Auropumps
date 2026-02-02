22599e3c062bcdefb8d82408df3cee43bb49a65c
Chaitali More <chaitali@dotscoms.com>
2026-02-02 11:56:04 +0530

    Poweradmin

--- Poweradmin.Server/obj/Debug/net8.0/Poweradmin.Server.AssemblyInfo.cs
+++ Poweradmin.Server/obj/Debug/net8.0/Poweradmin.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Poweradmin.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7dcec62507998ec2e79c32cea0713033956385f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Poweradmin.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Poweradmin.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

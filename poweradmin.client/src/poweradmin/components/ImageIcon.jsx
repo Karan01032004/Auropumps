c6e9bff4922c81ba8878d84a532c3b1807356668
Karan01032004 <karan@dotscoms.com>
2026-03-13 13:56:43 +0530

    Delete poweradmin.client directory

--- poweradmin.client/src/poweradmin/components/ImageIcon.jsx
+++
@@ -1,9 +0,0 @@
-const ImageIcon = ({ src, alt = "", className = "" }) => (
-    <img
-        src={src}
-        alt={alt}
-        className={`w-5 h-5 object-contain ${className}`}
-    />
-);
-
-export default ImageIcon;
